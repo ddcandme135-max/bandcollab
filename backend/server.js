2c33f212bcc44e3c0d671107bb4494bb2f4a4d14
ddcandme135-max <ddcandme135@gmail.com>
2026-03-13 18:21:27 +0900

    server.js 2

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-// BandCollabo API server
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("BandCollabo API running");
+});
+
+app.get("/projects", (req, res) => {
+  res.json({
+    message: "Projects endpoint working"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
